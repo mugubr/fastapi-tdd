a6bd4b6f4822cdd1514ae26148183357eea29017
mugubr <miguimuniz@gmail.com>
2023-12-06 18:01:45 -0300

    feat: iniciando testes de use case

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import asyncio
+from uuid import UUID
 import pytest
 from store.db.mongo import db_client
+from store.schemas.product import ProductIn
+from tests.factories import product_data
 
 
 @pytest.fixture(scope="session")
@@ -24,3 +27,13 @@ async def clear_collections(mongo_client):
         if collection_name.startswith("system"):
             continue
         await mongo_client.get_database()[collection_name].delete_many({})
+
+
+@pytest.fixture
+def product_id() -> UUID:
+    return UUID("fce6cc37-10b9-4a8e-a8b2-977df327001a")
+
+
+@pytest.fixture
+def product_in(product_id):
+    return ProductIn(**product_data(), id=product_id)
